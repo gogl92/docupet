50662b25ee0de9a048a73f32e6cff86bcaad5c67
Luis Gonzalez <luisarmando1234@gmail.com>
2024-01-22 01:52:01 -0500

    :card_file_box: Adding Models

---
+++ app/Models/Pet.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $name
+ * @property string $type
+ * @property string $breed
+ * @property string $gender
+ */
+class Pet extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'type',
+        'breed',
+        'gender',
+    ];
+}
